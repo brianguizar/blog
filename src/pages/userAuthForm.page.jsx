a80c019146fa92ac083f22063db81bcb05c38956
daniel33870 <daniel33870@gmail.com>
2024-09-26 16:17:01 -0600

    Arreglo Carpetas

--- src/pages/userAuthForm.page.jsx
+++ src/pages/userAuthForm.page.jsx
@@ -1,76 +1,152 @@
-import InputBox from "../components/input.component";
-import googleIcon from "../imgs/google.png"; // Icono de Google que se usará en el botón de iniciar sesión con Google
+import { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
-import AnimationWrapper from "../common/page-animation"; // Componente para animar la página
+import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png";
+import AnimationWrapper from "../../common/page-animation";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+import { storeInSession } from "../../common/session";
+import { UserContext } from "../App";
 
-// UserAuthForm, "type" que indica si es para iniciar sesión o registrarse
 const UserAuthForm = ({ type }) => {
+  const authForm = useRef();
+
+  let {
+    userAuth: { access_token },
+    setUserAuth,
+  } = useContext(UserContext);
+
+  console.log(access_token);
+
+  const userAuthThroughServer = (serverRoute, formData) => {
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
+      .then(({ data }) => {
+        storeInSession("user", JSON.stringify(data));
+        console.log(sessionStorage);
+      })
+      .catch(({ response }) => {
+        toast.error(response.data.error);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex para el correo
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex para la contraseña
+
+    let form = new FormData(formElement);
+    let formData = {};
+
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
+    }
+
+    let { fullname, email, password } = formData;
+    // Validación de los datos
+    if (fullname) {
+      if (fullname.length < 3) {
+        return toast.error("Tu nombre debe ser más largo que 3 letras");
+      }
+    }
+
+    if (!email.length) {
+      return toast.error("Ingresa un correo");
+    }
+    if (!emailRegex.test(email)) {
+      return toast.error("Email inválido");
+    }
+    if (!passwordRegex.test(password)) {
+      return toast.error(
+        "La contraseña debe tener de 6 a 20 caracteres, con 1 número, 1 minúscula y 1 mayúscula"
+      );
+    }
+
+    userAuthThroughServer(serverRoute, formData);
+  };
   return (
-    // Componente de animación
     <AnimationWrapper keyValue={type}>
       <section className="h-cover flex items-center justify-center">
-        <form className="w-[80%] max-w-[400px]">
-          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-            {type == "sign-in" ? "Bienvenido" : "Registrate"}
+        <Toaster />
+        <form id="formElement" className="w-[80%] max-w-[400px]">
+          {/*Validacion del tipo de formulario*/}
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-8">
+            {type == "sign-in" ? "Iniciar Sesion" : "Registrate"}
           </h1>
 
-          {/* Si el tipo no es "sign-in", mostramos el campo de nombre completo */}
-          {type != "sign-in" ? (
-            <InputBox
-              name="fullname"
-              type="text"
-              placeholder="Ingresa tu nombre"
-              icon="fi-rr-user" // Icono de usuario
-            />
-          ) : (
-            ""
-          )}
+          {
+            //Se añade una validacion para cque en caso de que sea Registro se añada el input del nombre
+            type != "sign-in" ? (
+              <InputBox
+                name="fullname"
+                type="text"
+                placeholder="Ingresa tu nombre"
+                // Solo se toma la parte de los asteriscos <i class="fi **********"></i>
+                icon="fi-rr-user"
+              />
+            ) : (
+              ""
+            )
+          }
 
-          {/* Campo de correo electrónico que siempre se muestra */}
           <InputBox
             name="email"
             type="email"
             placeholder="Ingresa tu correo"
-            icon="fi-rr-envelope" // Icono de sobre para correo
+            icon="fi-rr-envelope"
           />
 
-          {/* Campo de contraseña que siempre se muestra */}
           <InputBox
             name="password"
             type="password"
             placeholder="Ingresa tu contraseña"
-            icon="fi-rr-key" // Icono de llave para contraseña
+            icon="fi-rr-key"
           />
 
-          {/* Botón de enviar que cambia su texto según el tipo (iniciar sesión o registrarse) */}
-          {type == "sign-in" ? (
-            <button className="btn-dark center mt-14" type="submit">
-              {type.replace("sign-in", "Iniciar sesion")}{" "}
-              {/* Reemplaza el texto "sign-in" por "Iniciar sesión" */}
-            </button>
-          ) : (
-            <button className="btn-dark center mt-14" type="submit">
-              {type.replace("sign-up", "Registrarme")}{" "}
-              {/* Reemplaza el texto "sign-up" por "Registrarme" */}
-            </button>
-          )}
+          {
+            //Seria mas facil en ingles pero bueno
+            type == "sign-in" ? (
+              <button
+                className="btn-dark center mt-14"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                {type.replace("sign-in", "Iniciar sesion")}
+              </button>
+            ) : (
+              <button
+                className="btn-dark center mt-14"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                {type.replace("sign-up", "Registrarme")}
+              </button>
+            )
+          }
 
-          {/* División línea horizontal y el texto "O" en el medio */}
-          <div className="relative w-full flex items-center gap-2 my-10 opacity-50 uppercase text-black font-bold">
+          {/*Validacion en Ingles
+                        <button 
+                            className="btn-dark center mt-14"
+                        >
+                            { type.replace("-", " ") }
+                        </button>
+                    */}
+
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-0 uppercase text-black font-bold">
             <hr className="w-1/2 border-black" />
-            <p>O</p>
+            <p>or</p>
             <hr className="w-1/2 border-black" />
           </div>
 
-          {/* Botón para iniciar sesión con Google, que incluye el icono de Google */}
           <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-            <img src={googleIcon} className="w-5" /> {/* Icono de Google */}
+            <img src={googleIcon} className="w-5" />
             Continuar con Google
           </button>
 
-          {/* Mensaje condicional que cambia dependiendo de si es "sign-in" o "sign-up" */}
           {type == "sign-in" ? (
-            // Si es "sign-in", mostramos el enlace para registrarse
             <p className="mt-6 text-dark-grey text-xl text-center">
               ¿Aun no tienes una cuenta?
               <Link to="/signup" className="underline text-black text-xl ml-1">
@@ -78,7 +154,6 @@ const UserAuthForm = ({ type }) => {
               </Link>
             </p>
           ) : (
-            // Si es "sign-up", mostramos el enlace para iniciar sesión
             <p className="mt-6 text-dark-grey text-xl text-center">
               ¿Ya tienes una cuenta?
               <Link to="/signin" className="underline text-black text-xl ml-1">
