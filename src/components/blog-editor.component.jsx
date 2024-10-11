f8c629fc044d26eca6ded90eb6fc3e82f83460f1
daniel33870 <daniel33870@gmail.com>
2024-10-11 14:51:54 -0600

    Se agrego el Context del Blog

--- src/components/blog-editor.component.jsx
+++ src/components/blog-editor.component.jsx
@@ -2,12 +2,32 @@ import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
+import { useContext } from "react";
+import { EditorContext } from "../pages/editor.pages";
 
 const BlogEditor = () => {
+
+  let { blog, blog: { title, banner, content, tags, des  }, setBlog } = useContext(EditorContext)
+
   const handleBannerUpload = (e) => {
     let img = e.target.files(0);
-    console.log(img);
+    console.log(img)  
   };
+
+  const handleTitleKeyDown = (e) => {
+    if(e.keyCode == 13){
+      //enter prohibido
+      e.preventDefault();
+    }
+  }
+
+  const handleTitleChange = (e) => {
+    let input = e.target;
+    input.style.height = 'auto';
+    input.style.height = input.scrollHeight + "px"
+
+    setBlog({ ...blog, title: input.value })
+  }
 
   return (
     <>
@@ -16,7 +36,7 @@ const BlogEditor = () => {
           <img src={logo} />
         </Link>
         <p className="max-md:hidden text-black line-clamp-1 w-full">
-          Nuevo Blog
+          { title.length ? title : "New blog" }
         </p>
 
         <div className="flex gap-4 ml-auto">
@@ -40,6 +60,12 @@ const BlogEditor = () => {
                 />
               </label>
             </div>
+            <textarea
+              placeholder="Titulo del Blog"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+            ></textarea>
           </div>
         </section>
       </AnimationWrapper>
