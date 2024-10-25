0389dafab2006b7222ae00e41d866c23acccdc03
brianguizar <134902036+brianguizar@users.noreply.github.com>
2024-10-25 13:09:46 -0600

    Se agrego la vista a la pagina principla

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import UserAuthForm from "./pages/userAuthForm.page";
 import { createContext, useEffect, useState } from "react";
 import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
+import HomePage from "./pages/home.page";
 
 // Creación del contexto para compartir información del usuario a través de la aplicación
 export const UserContext = createContext({});
@@ -26,6 +27,7 @@ const App = () => {
       <Routes>
         <Route path="/editor" element={<Editor />} />
         <Route path="/" element={<Navbar />}>
+          <Route  index element = {<HomePage />}/>
           <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
           <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
         </Route>
