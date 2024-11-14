913fe5f94b5ab989fb02bc28405cac105b21e987
brianguizar <134902036+brianguizar@users.noreply.github.com>
2024-11-14 17:17:20 -0600

    Vista de las busquedas

---
+++ src/pages/search.page.jsx
@@ -0,0 +1,106 @@
+import { useParams } from "react-router-dom";
+import InPageNavigation from "../components/inpage-navigation.component";
+import { useEffect, useState } from "react";
+import Loader from "../components/loader.component";
+import AnimationWrapper from "../common/page-animation";
+import BlogPostCard from "../components/blog-post.component";
+import NoDataMessage from "../components/nodata.component";
+import LoadMoreDataBtn from "../components/load-more.component";
+import { filterPaginationData } from "../common/filter-pagination-data";
+import axios from 'axios';
+
+
+const SearchPage = () =>{
+
+    let { query } = useParams()
+
+    let [blogs, setBlog ] =useState(null);
+
+    const searchBlogs = ({page = 1, create_new_arr = false  }) =>{
+
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", {query, page})
+            .then(async ({ data }) => {
+
+          
+                console.log(data.blogs);
+        
+                let formatedData = await filterPaginationData({
+        
+                  state : blogs,
+                  data: data.blogs,
+                  page,
+                  countRoute : "/search-blogs-count",
+                  data_to_send: { query },
+                  create_new_arr
+        
+        
+                }) 
+                   
+                   console.log(formatedData);
+                   setBlog(formatedData);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+        
+    }
+
+    useEffect(() => {
+        resetState();
+        searchBlogs({page : 1, create_new_arr : true});
+    }, [query])
+
+    const resetState = () =>{
+
+        setBlog(null);
+
+
+    }
+
+    return (
+
+        <section className="h-cover felx justify-center gap-10 ">
+
+            <div className="w-full">
+
+                <InPageNavigation routes={[`Resultados de la busqueda de "${query}"`,"Cuentas encontradas"]}
+                 defaultHidden={["Cuentas encontradas"]}>
+
+                    <>
+                             {blogs == null ? (
+                                <Loader />
+                              ) : (
+                                blogs.results.length ? 
+                                  blogs.results.map((blog, i) => {
+                                    return(
+                                    <AnimationWrapper
+                                      transition={{ duration: 1, delay: i * 0.1
+                                      }}
+                                      key={i}
+                                    >
+                                      <BlogPostCard
+                                        content={blog}
+                                        author={blog.author.personal_info
+                
+                                        }
+                                      />
+                                    </AnimationWrapper>
+                                  );
+                                })
+                                :<NoDataMessage message="No hay blogs publicados"/>
+                              )}
+                              <LoadMoreDataBtn state={blogs} fetchDataFun={searchBlogs}/>
+                    </>
+
+
+                </InPageNavigation>
+
+            </div>
+
+
+        </section>
+       
+    )
+}
+
+export default SearchPage;
