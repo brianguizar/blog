9220540a49531a835e442b249af0f32cad5fbb87
brianguizar <134902036+brianguizar@users.noreply.github.com>
2024-11-27 16:01:27 -0600

    Se agrego la funcion de notificaciones

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,19 +1,45 @@
 import { Link, Outlet, useNavigate } from "react-router-dom"; 
 import logo from "../imgs/logo.png"; 
-import { useContext, useState } from "react"; 
+import { useContext, useState, useEffect } from "react"; 
 import { UserContext } from '../App';
 import UserNavigationPanel from "./user-navigation.component";
+import axios from "axios";
 
 const Navbar = () => {
     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
     const [userNavPanel, setUserNavPanel] = useState(false);
-    const { userAuth } = useContext(UserContext);
-    const { access_token, profile_img } = userAuth || {}; // Verifica si userAuth está definido
+    const { userAuth,userAuth:{access_token, profile_img, new_notification_available}, setUserAuth } = useContext(UserContext);
+  
+    
     let navigate = useNavigate();
 
     const handleUserNavPanel = () => {
         setUserNavPanel(currentVal => !currentVal);
     };
+
+    useEffect(() => {
+
+        if(access_token){
+
+            axios.get(import.meta.env.VITE_SERVER_DOMAIN + 
+                "/new-notification", {
+
+                    headers : {
+
+                        'Authorization' : `Bearer ${access_token}`
+                    }
+                })
+                .then(({data}) => {
+
+                    setUserAuth({...userAuth, ...data})
+                })
+                .catch((err => {
+                    console.log(err)
+                }))
+        }
+
+
+    }, [access_token])
 
     const handleSearch = (e) => {
         let query = e.target.value;
@@ -34,6 +60,8 @@ const Navbar = () => {
                 <Link to="/" className="flex-none w-10">
                     <img src={logo} className="w-full" alt="Logo" />
                 </Link>
+
+                <p>{new_notification_available}</p>
 
                 {/* Barra de búsqueda */}
                 <div className={`absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto ${searchBoxVisibility ? "" : "hidden"}`}>
@@ -62,6 +90,12 @@ const Navbar = () => {
                             <Link to="/dashboard/notification">
                                 <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
                                     <i className="fi fi-rr-bell text-2x1 block mt-1"></i>
+                                    {
+                                        new_notification_available ?
+                                        <span className="bg-red w-3 h-3 rounded-full absolute z-10 top-2 right-2">                                            
+                                        </span> : ""
+
+                                    }
                                 </button>
                             </Link>
 
